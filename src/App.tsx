cb0726453e668f0b7f628ba2435621f96542540d
Nikita <sychev_n83@mail.ru>
2020-11-18 15:10:31 +0500

    prepare to publish in npmjs.com

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import './App.css';
-import SoundPlayer from "./components/sound-player/sound-player";
+import SoundPlayer from "./node_modules/components/sound-player/sound-player";
 // @ts-ignore
 import attSnd from './sound/attention.wav';
 // @ts-ignore
@@ -24,6 +24,7 @@ import aSnd from './sound/letters/a.wav';
 // @ts-ignore
 import xSnd from './sound/letters/x.wav';
 
+
 const App: React.FunctionComponent = (): React.ReactElement => {
     const [sounds, setSounds] = useState<string[]>([]);
 
@@ -45,10 +46,14 @@ const App: React.FunctionComponent = (): React.ReactElement => {
     };
     return (
         <React.Fragment>
-            <h1>{'This is sound player'}</h1>
-            <button onClick={() => playSounds('first')}>{'play first'}</button>
-            <button onClick={() => playSounds('second')}>{'play second'}</button>
-            <button onClick={() => playSounds('third')}>{'play third'}</button>
+            <div style={{margin: 15}}>
+                <h1>{'This is sound player'}</h1>
+                <div style={{width: 250, display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
+                    <button onClick={() => playSounds('first')}>{'play first'}</button>
+                    <button onClick={() => playSounds('second')}>{'play second'}</button>
+                    <button onClick={() => playSounds('third')}>{'play third'}</button>
+                </div>
+            </div>
             <SoundPlayer isExternalSound={false} sounds={sounds} onFinished={soundFinish}/>
         </React.Fragment>
     );
